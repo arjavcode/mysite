5f3781b81790f7dccf5e367d30fcb00f8855e659
Arjav Jain <luckyjain1271@gmail.com>
2022-07-04 00:06:31 +0530

    so added forms section

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,10 @@
 from ast import Try
 from multiprocessing import context
-from django.shortcuts import render
-from django.http import HttpResponse, Http404
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
-from .models import Question
+from .models import Choice, Question
 # Create your views here.
 
 def index(request):
@@ -14,14 +15,22 @@ def index(request):
     return render(request , 'polls/index.html' , context)
 
 def detail(request , question_id):
-    try:
-        question = Question.objects.get(id = question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question Doesn't Exist")
+    question = get_object_or_404(Question , pk = question_id)
     return render(request , 'polls/detail.html' , { 'question':question})
 
 def results(request , question_id):
-    return HttpResponse("You'r looking at the results of question %s" %question_id)
+    question = get_object_or_404(Question, pk = question_id)
+    return render ( request, 'polls/results.html', {'question': question} )
+
 
 def vote(request , question_id):
-    return HttpResponse("You'r voting on question %s" %question_id)
+    question = get_object_or_404(Question, pk = question_id)
+    try:
+        selected_choice = question.choice_set.get(pk = request.POST['choice'])
+    except (KeyError , Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {'question':question, 'error_message':"You didn't select a Choice"})
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results' , args=(question.id,)))
+        
